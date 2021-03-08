7460e9858d172e880be1a86deea694cbdc165516
Payal Naik <payalnaik97@gmail.com>
2021-03-08 23:30:16 +0530

    Second Commit

--- TestData/data.ts
+++ TestData/data.ts
@@ -2,7 +2,7 @@ export default {
     appUrl: "http://petclinicui.e46708b92c054086909b.eastus.aksapp.io/petclinic/",
     userData: {
         OwnerData: {
-            ownerDetails: "Ernesto Wayne 4920 Ingram Street Dayton 8595874250"
+            ownerDetails: "Ernesteo Wayne 4920 Ingram Street Dayton 8595874250"
         },
     }
 }
